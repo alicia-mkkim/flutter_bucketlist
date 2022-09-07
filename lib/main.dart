68fbb9d04fe6826a8468b3465d9ff47fb01918d7
alicia.kim <alcia.mkkkim@gmail.com>
2022-09-07 18:40:37 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Bucket {
+  String job;
+  bool isDone;
+  Bucket(this.job, this.isDone);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BucketList',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //버킷리스트
+  List<Bucket> bucketList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('버킷리스트'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: bucketList.isEmpty
+          ? Center(
+              child: Text('버킷리스트를 작성해주세요!'),
+            )
+          : ListView.builder(
+              itemCount: bucketList.length,
+              itemBuilder: (context, index) {
+                Bucket bucket = bucketList[index];
+
+                return ListTile(
+                  title: Text(
+                    bucket.job,
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: bucket.isDone ? Colors.grey : Colors.black,
+                        decoration: bucket.isDone
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {
+                        showDeleteDialog(context, index);
+                      },
+                      icon: Icon(CupertinoIcons.delete)),
+                  onTap: () {
+                    setState(() {
+                      bucket.isDone = !bucket.isDone;
+                    });
+                  },
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          String? job = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => CreatePage()),
+          );
+          if (job != null) {
+            setState(() {
+              Bucket newBucket = new Bucket(job, false);
+              bucketList.add(newBucket);
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  void showDeleteDialog(BuildContext context, int index) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('삭제하시겠습니까?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('취소'),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    // index에 해당하는 항목 삭제
+                    bucketList.removeAt(index);
+                  });
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  '확인',
+                  style: TextStyle(color: Colors.pinkAccent),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+}
+
+class CreatePage extends StatefulWidget {
+  const CreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  TextEditingController textController = TextEditingController();
+  String? error;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("버킷리스트 작성"),
+        leading: IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: '하고 싶은 일을 입력하세요!',
+                errorText: error,
+              ),
+            ),
+            SizedBox(height: 32),
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                child: Text(
+                  '추가하기',
+                  style: TextStyle(fontSize: 18),
+                ),
+                onPressed: () {
+                  // 추가하기 버튼 클릭시
+                  String job = textController.text;
+                  if (job.isEmpty) {
+                    setState(() {
+                      error = '에러: 내용을 입력해주세요';
+                    });
+                  } else {
+                    setState(() {
+                      error = null;
+                    });
+                  }
+                  Navigator.pop(context, job);
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
